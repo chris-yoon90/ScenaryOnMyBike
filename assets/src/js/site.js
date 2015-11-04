398bdbbef547142c99a8d77555561ff4d7efa79e
Chris Yoon <chris.yoon90@gmail.com>
2015-11-03 19:23:33 -0800

    refactoring some css and js

--- assets/src/js/site.js
+++ assets/src/js/site.js
@@ -1,31 +1,9 @@
 $(function() {
-	var NAV_HEIGHT = 80;
-	var $header = $('.main-header');
-	var $site_header = $('.site-header');
-	var $navbar = $('.navbar-container');
+	scrollHandler();
+	$(window).resize(_util.debounce(scrollHandler, 250));
+	$(window).scroll(_util.throttle(scrollHandler, 100))
 
-	darkenSiteHeaderBackgroundColour();
-	$(window).resize(_util.debounce(darkenSiteHeaderBackgroundColour, 250));
-
-	function darkenSiteHeaderBackgroundColour() {
-		if($header.length > 0) {
-			$(window).scroll(_util.throttle(scrollHandler, 100));
-		}
-
-		function scrollHandler() {
-			var offsetFromTop = $(window).scrollTop();
-			var headerHeight = $header.outerHeight();
-
-			if(offsetFromTop + NAV_HEIGHT > headerHeight) {
-				$site_header.addClass('darken');
-				return;
-			}
-
-		$site_header.removeClass('darken');
-		}
-	}
-
-	$navbar.find('.navbar .nav-blog').click(function(e) {
+	$('.navbar-container').find('.navbar .nav-blog').click(function(e) {
 		if(!App.showBlog() && App.isHomeContext()) {
 			App.hideCoverAndShowBlog();
 		} else if(App.showBlog() && App.isHomeContext()){
@@ -37,5 +15,15 @@ $(function() {
 	if(App.showBlog() && App.isHomeContext()) {
 		App.hideCoverAndShowBlog();
 	}
-	
+
+	function scrollHandler() {
+		var offsetFromTop = $(window).scrollTop();
+
+		if(offsetFromTop > 0) {
+			$('.site-header').addClass('darken');
+			return;
+		}
+
+		$('.site-header').removeClass('darken');
+	}
 });
