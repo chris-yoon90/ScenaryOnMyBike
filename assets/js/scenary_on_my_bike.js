988037be256fa35f3cbea80dc4a57af9377da471
Chris Yoon <chris.yoon90@gmail.com>
2015-11-19 23:48:09 -0800

    restyle site nav toggle button

--- assets/js/scenary_on_my_bike.js
+++ assets/js/scenary_on_my_bike.js
@@ -247,8 +247,6 @@ $(document).ready(function() {
 });;"use strict";
 (function() {
 	$(document).ready(function() {
-		scrollHandler();
-		$(window).scroll(_util.throttle(scrollHandler, 100))
 
 		$('.navbar-container').find('.navbar .nav-blog').click(function(e) {
 			if(!App.showBlog() && App.isHomeContext()) {
@@ -273,15 +271,15 @@ $(document).ready(function() {
 	$(window).load(App.reLayoutMasonry);
 
 	function toggleSideNav(e) {
-		$('.side-nav').toggleClass('active');
-		$('.mask-modal').toggleClass('active');
+		$('.side-nav, .mask-modal, .site-header').toggleClass('active');
+		$('.nav-toggle-btn').toggleClass('close');
 		$('body').toggleClass('no-overflow');
 
 		if($('.side-nav').hasClass('active')) {
 			$('.mask-modal').on('click.nav-bar-deactivate', function(event) {
 				if(!$('.side-nav').is(event.target) && !$('.side-nav').has(event.target).length) {
-					$('.side-nav').removeClass('active');
-					$('.mask-modal').removeClass('active');
+					$('.side-nav, .mask-modal, .site-header').removeClass('active');
+					$('.nav-toggle-btn').removeClass('close');
 					$('body').removeClass('no-overflow');
 					$('.mask-modal').off('click.nav-bar-deactivate');
 				}
@@ -289,17 +287,6 @@ $(document).ready(function() {
 		} else {
 			$('.mask-modal').off('click.nav-bar-deactivate');
 		}
-	}
-
-	function scrollHandler() {
-		var offsetFromTop = $(window).scrollTop();
-
-		if(offsetFromTop > 0) {
-			$('.site-header').addClass('box-shadow-1');
-			return;
-		}
-
-		$('.site-header').removeClass('box-shadow-1');
 	}
 })();
 
